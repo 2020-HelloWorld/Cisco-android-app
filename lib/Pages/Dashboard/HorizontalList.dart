b191ce22920474370e7fb1243ed8ad4da86e4611
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-08-25 19:39:52 +0530

    Brand Dashboard

--- lib/Pages/Dashboard/HorizontalList.dart
+++ lib/Pages/Dashboard/HorizontalList.dart
@@ -12,13 +12,82 @@ class HorizontalList extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             itemBuilder: (context,index)
             {
-              return Container(
-                margin: EdgeInsets.only(left: 30),
-                width: 125,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10)
+              return Stack(
+                children:[
+                Container(
+                  margin: EdgeInsets.only(left: 30),
+                  width: 125,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: FittedBox(
+                    child: ClipRRect(
+                      borderRadius:BorderRadius.circular(10),
+                        child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwMTRVUGAGKSV-zYdlhlGBx3pUsnWZhcyQBA&usqp=CAU"),
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+
                 ),
+                  Padding(
+                    padding: const EdgeInsets.only(left:30.0),
+                    child: Container(
+                      height: 45,
+                      width: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.4),
+                          borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left:4.0),
+                            child: Text(
+                              "9",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black.withOpacity(0.55)
+                              ),
+                            ),
+                          ),
+                          Icon(Icons.star,color: Colors.black.withOpacity(0.55))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:140.0,left:30),
+                    child: Container(
+                      height: 35,
+                      width: 125,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.4),
+                          borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left:15.0),
+                            child: Icon(Icons.airplay_outlined,color: Colors.black.withOpacity(0.55)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left:8.0),
+                            child: Text(
+                              "110K",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black.withOpacity(0.55)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ]
               );
             }
         )
