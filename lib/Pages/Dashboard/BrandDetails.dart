28701fadd87200c50d79e1786bfc9ddd98e60a90
CABREX <asvamogh@gmail.com>
2022-10-13 23:54:08 +0530

    Fixed routing and network assets issues

--- lib/Pages/Dashboard/BrandDetails.dart
+++ lib/Pages/Dashboard/BrandDetails.dart
@@ -20,72 +20,71 @@ class _BrandDetailsState extends State<BrandDetails> {
       backgroundColor: Color(0xffEBF3FF),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Expanded(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 18),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 115),
-                      Image.asset("assets/images/logo.png",
-                          width: 145, height: 43),
-                      //try providing asset img.
-                      SizedBox(width: 66.52),
-                      Icon(
-                        Icons.notifications,
-                        color: Color(0xff354044),
-                        size: 28,
-                      ),
-                    ],
+          // Expanded was removed.
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 18),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 115),
+                    Image.asset("assets/images/logo.png",
+                        width: 145, height: 43),
+                    //try providing asset img.
+                    SizedBox(width: 66.52),
+                    Icon(
+                      Icons.notifications,
+                      color: Color(0xff354044),
+                      size: 28,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: 480,
+                child: ListTile(
+                  leading: Icon(Icons.search),
+                  title: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Search an Influencer",
+                        border: InputBorder.none),
+                  ),
+                  trailing: Icon(Icons.filter_alt_rounded),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [Colors.grey.shade300, Colors.white10],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0, 0.4],
+                    tileMode: TileMode.clamp,
                   ),
                 ),
-                SizedBox(height: 30),
-                Container(
-                  width: 480,
-                  child: ListTile(
-                    leading: Icon(Icons.search),
-                    title: TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Search an Influencer",
-                          border: InputBorder.none),
-                    ),
-                    trailing: Icon(Icons.filter_alt_rounded),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      colors: [Colors.grey.shade300, Colors.white10],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      stops: [0.0, 0.4],
-                      tileMode: TileMode.clamp,
-                    ),
-                  ),
+              ),
+              SizedBox(height: 31),
+              Container(
+                height: 900,
+                child: Column(
+                  children: List.generate(infl.length, (index) {
+                    return BrandDetailsCont(
+                      index: index,
+                    );
+                  }),
                 ),
-                SizedBox(height: 31),
-                Container(
-                  height: 900,
-                  child: Column(
-                    children: List.generate(infl.length, (index) {
-                      return BrandDetailsCont(
-                        index: index,
-                      );
-                    }),
-                  ),
-                  // child: ListView.builder(
-                  //   itemCount: 10,
-                  //   scrollDirection: Axis.vertical,
-                  //   itemBuilder: (context, index) {
-                  //     return BrandDetailsCont();
-                  //   },
-                  // ),
-                )
-              ],
-            ),
+                // child: ListView.builder(
+                //   itemCount: 10,
+                //   scrollDirection: Axis.vertical,
+                //   itemBuilder: (context, index) {
+                //     return BrandDetailsCont();
+                //   },
+                // ),
+              )
+            ],
           ),
         ),
       ),
